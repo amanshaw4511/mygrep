512aa448a4a6678ddd4de6d7afb338000a53ca19
aman shaw <amansahw4511@gmail.com>
2021-05-04 18:58:29 +0530

    added pattern matching
    added color output

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,43 @@
 
 use clap::{App, Arg, ArgMatches};
+use regex::Regex;
+use std::io::{self, Write};
+use anyhow::{Context, Result};
+use colored::*;
 
-pub fn main() {
-    let matches = fun_name();
+pub fn main() -> Result<()>{
+    let matches = parse_commandline_args();
 
     let pattern = matches.value_of("pattern").unwrap();
     let path = matches.value_of("file").unwrap();
+    let is_color = matches.is_present("color");
 
-    let contents = std::fs::read_to_string(&path).expect("file not found");
+    let pattern = Regex::new(pattern)
+        .with_context(|| "invalid pattern")?;
+
+    let contents = std::fs::read_to_string(&path)
+        .with_context(|| format!("could not read file '{}'", path))?;
+
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
 
     for line in contents.lines() {
-        if line.contains(&pattern) {
-            println!("{}", line);
+        if let Some(matched) = pattern.find(line) {
+            if is_color {
+                writeln!(handle, "{}{}{}",
+                         &line[..matched.start()],
+                         &line[matched.start()..matched.end()].red(),
+                         &line[matched.end()..])?;
+            } else {
+                writeln!(handle, "{}", line)?; 
             }
+        }
     }
+
+    Ok(())
 }
 
-fn fun_name() -> ArgMatches {
+fn parse_commandline_args() -> ArgMatches {
     App::new("mygrep")
         .version("1.0.0")
         .author("Aman Shaw  <amanshaw4511@protonmail.com>")
@@ -28,7 +49,7 @@ fn fun_name() -> ArgMatches {
              .about("regex pattern to find")
              .takes_value(true)
              .required(true)
-             )
+            )
         .arg(Arg::new("file")
              .short('f')
              .long("file")
@@ -36,6 +57,13 @@ fn fun_name() -> ArgMatches {
              .about("file to find for matches")
              .takes_value(true)
              .required(true)
-             )
+            )
+        .arg(Arg::new("color")
+             .short('c')
+             .long("color")
+             .takes_value(false)
+             .about("prints matching string in color")
+             .required(false)
+            )
         .get_matches()
 }
