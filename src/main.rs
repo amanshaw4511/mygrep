dcb314ffe3c0deac5f3008123cd67bb65a6c0b74
aman shaw <amansahw4511@gmail.com>
2021-05-05 00:22:50 +0530

    added ignorecase and invert-match

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
 
 use clap::{App, Arg, ArgMatches};
-use regex::Regex;
-use std::io::{self, Write};
+use regex::{Match, Regex};
+use std::io::{self, StdoutLock, Write};
 use anyhow::{Context, Result};
 use colored::*;
 
 pub fn main() -> Result<()>{
-    let matches = parse_commandline_args();
+    let args = parse_commandline_args();
 
-    let pattern = matches.value_of("pattern").unwrap();
-    let path = matches.value_of("file").unwrap();
-    let is_color = matches.is_present("color");
+    let pattern = args.value_of("pattern").unwrap();
+    let path = args.value_of("file").unwrap();
+    let has_color = args.is_present("color");
+    let has_ignore_case = args.is_present("ignore_case");
+    let has_invert_match = args.is_present("invert_match");
 
-    let pattern = Regex::new(pattern)
-        .with_context(|| "invalid pattern")?;
+
+    let ignore_case_pattern = format!("(?i){}", pattern);
+
+    let pattern = Regex::new(match has_ignore_case 
+        {
+            true => &ignore_case_pattern,
+            false => pattern
+        }).with_context(|| "invalid pattern")?;
 
     let contents = std::fs::read_to_string(&path)
         .with_context(|| format!("could not read file '{}'", path))?;
@@ -22,16 +30,33 @@ pub fn main() -> Result<()>{
     let mut handle = stdout.lock();
 
     for line in contents.lines() {
-        if let Some(matched) = pattern.find(line) {
-            if is_color {
-                writeln!(handle, "{}{}{}",
-                         &line[..matched.start()],
-                         &line[matched.start()..matched.end()].red(),
-                         &line[matched.end()..])?;
-            } else {
-                writeln!(handle, "{}", line)?; 
+        match pattern.find(line) {
+            Some(matched)  => {
+                if !has_invert_match { 
+                    print_matched_line(&mut handle, line, matched, has_color)?;
+                };
+            },
+
+            None => {
+                if has_invert_match {
+                    writeln!(handle, "{}", line)?; 
+                };
             }
         }
+    }
+
+    Ok(())
+}
+
+#[inline]
+fn print_matched_line(handle: &mut StdoutLock,line: &str, matched: Match, has_color: bool) -> Result<()> {
+    if has_color {
+        writeln!(handle, "{}{}{}",
+                 &line[..matched.start()],
+                 &line[matched.start()..matched.end()].red(),
+                 &line[matched.end()..])?;
+    } else {
+        writeln!(handle, "{}", line)?; 
     }
 
     Ok(())
@@ -46,24 +71,38 @@ fn parse_commandline_args() -> ArgMatches {
              .short('p')
              .long("pattern")
              .value_name("PATTERN")
-             .about("regex pattern to find")
              .takes_value(true)
              .required(true)
+             .about("regex pattern to find")
             )
         .arg(Arg::new("file")
              .short('f')
              .long("file")
              .value_name("FILE")
-             .about("file to find for matches")
              .takes_value(true)
              .required(true)
+             .about("file to find for matches")
             )
         .arg(Arg::new("color")
              .short('c')
              .long("color")
              .takes_value(false)
-             .about("prints matching string in color")
              .required(false)
+             .about("prints matching string in color")
             )
+        .arg(Arg::new("ignore_case")
+             .short('i')
+             .long("ignore-case")
+             .takes_value(false)
+             .required(false)
+             .about("ignore matching case")
+            )
+        .arg(Arg::new("invert_match")
+             .short('v')
+             .long("invert-match")
+             .takes_value(false)
+             .required(false)
+             .about("prints not matching line")
+             )
         .get_matches()
 }
